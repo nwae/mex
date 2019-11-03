b278cf1904fb51d0c1e71277fc4880a5264a859b
mapktah <mapktah@yandex.ru>
2019-11-03 09:54:32 +0800

    first commit

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='mex',
+    version='1.0.0',
+    packages=[
+        'mex'
+    ],
+    package_dir={'': 'src'},
+    install_requires = [
+        'nwae.utils'
+    ],
+    url='',
+    license='',
+    author='nwae',
+    author_email='705270564@qq.com',
+    description='Match Expression'
+)
