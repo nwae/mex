0c1b231f9a989235a5a7538c2d7a52a23107e879
mapktah <mapktah@yandex.ru>
2019-11-20 16:25:20 +0800

    deploy scripts for mex api rest server

---
+++ deploy.scripts/deploy.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# Script name is the first parameter on command line
+SCRIPT_NAME="$0"
+
+########################################################################################
+# Modify only these variables
+########################################################################################
+# Where this script is relative to project directory
+PROGRAM_NAME="YOUR PROGRAM NAME"
+RUN_SCRIPT="./run.mex-api.sh"
+KILL_SCRIPT="./kill.by.port.sh"
+CLEANUP_SCRIPT=""
+LOGFILE_FOLDER=".."
+DO_TEST_AFTER_START=0
+# Config Files
+CONFIGFILE_LOCAL="../app.data/config/local.cf"
+CONFIGFILE_LIVE="../app.data/config/live.cf"
+CONFIGFILE_STAGING="../app.data/config/staging.cf"
+# Default Ports
+PORT_LOCAL=5000
+PORT_LIVE=5000
+PORT_STAGING=5000
+# Test Commands
+TEST_CMD_LOCAL="http://localhost:5000/mex?ret=1&pattern=m,float,dollar/dollars/make/$;y,int,year/yr&txt=my salary in year 2019 is 8888 dollars."
+TEST_CMD_LIVE="http://..."
+TEST_CMD_STAGING="http://..."
+################################################################################
+
+# Default config is nothing
+CF=
+CONFIGFILE=
+PORT=
+TEST_CMD=
+
+for keyvalue in "$@"; do
+    echo "[$SCRIPT_NAME] Key value pair [$keyvalue]"
+    IFS='=' # space is set as delimiter
+    read -ra KV <<< "$keyvalue" # str is read into an array as tokens separated by IFS
+    if [ "$KV" == "cf" ] ; then
+        CF="${KV[1]}"
+        if [ "$CF" == "local" ] ; then
+            CONFIGFILE="$CONFIGFILE_LOCAL"
+            PORT="$PORT_LOCAL"
+            TEST_CMD="$TEST_CMD_LOCAL"
+        elif [ "$CF" == "live" ] ; then
+            CONFIGFILE="$CONFIGFILE_LIVE"
+            PORT="$PORT_LIVE"
+            TEST_CMD="$TEST_CMD_LIVE"
+        elif [ "$CF" == "staging" ] ; then
+            CONFIGFILE="$CONFIGFILE_STAGING"
+            PORT="$PORT_STAGING"
+            TEST_CMD="$TEST_CMD_STAGING"
+        else
+          echo "[$SCRIPT_NAME] ERROR Invalid cf=$CF. Exiting script.."
+          exit 1
+        fi
+    fi
+done
+
+if [ "$CF" == "" ]; then
+  echo "[$SCRIPT_NAME] ERROR $PROGRAM_NAME Must specify config cf=[live, local, staging, ...]"
+  exit 1
+fi
+
+echo "[$SCRIPT_NAME] $PROGRAM_NAME For config $CF, set configfile to $CONFIGFILE, port to $PORT."
+
+#
+# First we do cleanup, better than setting in cron
+#
+if [ "$CLEANUP_SCRIPT" == "" ]; then
+  echo "[$SCRIPT_NAME] MISSING No cleanup script specified"
+else
+  if "$CLEANUP_SCRIPT"; then
+    echo "[$SCRIPT_NAME] OK. Log/etc files cleanup successful."
+  else
+    echo "[$SCRIPT_NAME] ERROR cleaning up log/etc files. Please do it manually."
+  fi
+fi
+sleep 0.5
+
+#
+# Kill Intent APIs wrapped in gunicorn
+#
+echo "[$SCRIPT_NAME] Try to kill $PROGRAM_NAME processes.."
+if ! "$KILL_SCRIPT" port="$PORT"; then
+  echo "[$SCRIPT_NAME] ERROR $PROGRAM_NAME Unable to kill existing processes.."
+  exit 1
+else
+  echo "[$SCRIPT_NAME] OK Successfully killed $PROGRAM_NAME processes."
+fi
+
+#
+# STDOUT & STDERR FILES PATH
+#
+STDOUTERRFILE="$LOGFILE_FOLDER/$CF.$PORT.outerr."`date +%Y-%m-%d_%H%M%S`".log"
+echo ""
+echo ""
+echo "[$SCRIPT_NAME] ***** RESTARTING $PROGRAM_NAME $CF PORT $PORT *****"
+echo "[$SCRIPT_NAME] Executing in folder $(pwd), configfile=$CONFIGFILE port=$PORT with out/err files '$STDOUTERRFILE'."
+if ! "$RUN_SCRIPT" \
+        configfile="$CONFIGFILE" \
+        port="$PORT" \
+        >"$STDOUTERRFILE" 2>&1 &
+then
+  echo "[$SCRIPT_NAME] ERROR Failed to run $PROGRAM_NAME."
+fi
+disown
+
+#
+# Sleep a little, then check stderr log for user to verify
+#
+echo "[$SCRIPT_NAME] Waiting for 5 secs to capture output from $PROGRAM_NAME startup.."
+sleep 5
+#
+# Ask user to review STDOUT / STDERR log
+#
+echo "[$SCRIPT_NAME] ********************* USER STDOUT / STDERR REVIEW ($PROGRAM_NAME STARTUP) *********************"
+tail -30 "$STDOUTERRFILE"
+echo "[$SCRIPT_NAME] Please review tail results (last 30 lines) from startup for stdout log ($STDOUTERRFILE) above."
+read -p "[$SCRIPT_NAME] Ok to proceed? (yes/no): " ok_to_proceed
+if [ "$ok_to_proceed" != "yes" ]; then
+  echo "[$SCRIPT_NAME] ERROR. User refuse to proceed after viewing stdout log. Exiting.."
+  exit 1
+fi
+
+#
+# Test server via curl
+#
+if [ $DO_TEST_AFTER_START -eq 1 ]; then
+  echo "[$SCRIPT_NAME] Doing tests on $PROGRAM_NAME via curl..."
+  echo "[$SCRIPT_NAME] Executing command curl -X GET $TEST_CMD"
+  curl -X GET "$TEST_CMD"
+  echo ""
+  echo "[$SCRIPT_NAME] ********************* USER AUTOMATIC TEST REVIEW ($PROGRAM_NAME STARTUP) *********************"
+  read -p "[$SCRIPT_NAME] Ok to proceed from above test result? (yes/no): " ok_to_proceed
+  if [ "$ok_to_proceed" != "yes" ]; then
+    echo "[$SCRIPT_NAME] ERROR. User refuse to proceed after viewing return value from automatic tests. Exiting.."
+    exit 1
+  fi
+fi
---
+++ deploy.scripts/run.mex-api.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# Script name is the first parameter on command line
+SCRIPT_NAME="$0"
+
+########################################################################################
+# Modify only these variables
+########################################################################################
+# Where this script is relative to project directory
+PROGRAM_NAME="Mex REST API"
+SCRIPT_DIR="deploy.scripts"
+COMPULSORY_CMDLINE_PARAMS=""
+PYTHON_VER="3.6"
+USE_GUNICORN=0
+GUNICORN_WORKERS=2
+# sync (CPU intensive), gthread (I/O intensive)
+GUNICORN_WORKER_TYPE="sync"
+GUNICORN_WORKER_TYPE_FLAG=""
+SOURCE_DIR="../src"
+COMPILE_MODULE="."
+MODULE_TO_RUN="mex.MexAPI"
+# Folders separated by ":"
+EXTERNAL_SRC_FOLDERS="../../nwae.utils/src"
+########################################################################################
+
+#
+# Command line parameters
+#
+PORT=
+CONFIGFILE=
+
+#
+# For gunicorn stuff
+#
+for keyvalue in "$@"; do
+    echo "[$SCRIPT_NAME] Key value pair [$keyvalue]"
+    IFS='=' # space is set as delimiter
+    read -ra KV <<< "$keyvalue" # str is read into an array as tokens separated by IFS
+    if [ "$KV" == "port" ] ; then
+        PORT="${KV[1]}"
+        echo "[$SCRIPT_NAME] Set port to $PORT."
+    elif [ "$KV" == "configfile" ] ; then
+        CONFIGFILE="${KV[1]}"
+        echo "[$SCRIPT_NAME] Set configfile to $CONFIGFILE."
+    fi
+done
+
+if [ "$CONFIGFILE" = "" ] && [ "$(echo "$COMPULSORY_CMDLINE_PARAMS" | grep -i "configfile")" != "" ]; then
+  echo "[$SCRIPT_NAME] ERROR Configfile not specified on command line. Exit 1."
+  exit 1
+fi
+if [ "$PORT" = "" ] && [ "$(echo "$COMPULSORY_CMDLINE_PARAMS" | grep -i "port")" != "" ]; then
+  echo "[$SCRIPT_NAME] ERROR Port not specified on command line. Exit 1."
+  exit 1
+fi
+
+#
+# Get RAM memory
+#
+# Default 1GB RAM
+RAM_MEMORY=1048576
+# Check if command "free" is available on system
+free 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+  echo "[$SCRIPT_NAME] WARNING. Could not determine RAM size.. defaulting to 1GB RAM."
+else
+  RAM_MEMORY=$(free| grep "^Mem" | sed s/"^Mem:[ \t]*"//g | sed s/"[ \t].*"//g)
+fi
+# Convert to gigabytes
+RAM_MEMORY_GB="$((RAM_MEMORY / (1024*1024)))"
+echo "[$SCRIPT_NAME] Total RAM $RAM_MEMORY_GB GB."
+
+#
+# This is the project directory
+#
+PROJECTDIR=$(pwd | sed s/[/]"$SCRIPT_DIR"//g)
+echo "[$SCRIPT_NAME] Using project directory $PROJECTDIR."
+
+for ext_src_folder in $(echo "$EXTERNAL_SRC_FOLDERS" | sed s/:/ /g) ; do
+  echo "[$SCRIPT_NAME] Checking external src directory $ext_src_folder"
+  if ! ls $ext_src_folder 1>/dev/null; then
+    echo "[$SCRIPT_NAME] No such directory $ext_src_folder"
+    exit 1
+  else
+    echo "[$SCRIPT_NAME] OK Directory $ext_src_folder"
+  fi
+done
+
+PYTHON_BIN=""
+FOUND=0
+#
+# Look for possible python paths
+#
+for path in "/usr/bin/python$PYTHON_VER" "/usr/local/bin/python$PYTHON_VER"; do
+    echo "[$SCRIPT_NAME] Checking python path $path.."
+
+    if ls $path 2>/dev/null 1>/dev/null; then
+        echo "[$SCRIPT_NAME]   OK Found python path in $path"
+        PYTHON_BIN=$path
+        FOUND=1
+        break
+    else
+        echo "[$SCRIPT_NAME]   ERROR No python in path $path"
+    fi
+done
+
+if [ $FOUND -eq 0 ]; then
+    echo "[$SCRIPT_NAME]   ERROR No python binary found!!"
+    exit 1
+fi
+
+GUNICORN_BIN=""
+FOUND=0
+#
+# Look for possible gunicorn paths
+#
+for path in "/usr/local/bin/gunicorn" "/Library/Frameworks/Python.framework/Versions/$PYTHON_VER/bin/gunicorn"
+do
+    echo "[$SCRIPT_NAME] Checking gunicorn path $path.."
+    if ls $path 2>/dev/null 1>/dev/null; then
+        echo "[$SCRIPT_NAME]   OK Found gunicorn path in $path"
+        GUNICORN_BIN=$path
+        FOUND=1
+        break
+    else
+        echo "[$SCRIPT_NAME]   ERROR No gunicorn in path $path"
+    fi
+done
+
+if [ $FOUND -eq 0 ]
+then
+    echo "   ERROR No gunicorn binary found!!"
+    exit 1
+fi
+
+
+#
+# Get command line params
+#
+echo "[$SCRIPT_NAME] Command line params: [$*]"
+
+#
+# Go into source directory
+#
+if ! cd $SOURCE_DIR ; then
+  echo "[$SCRIPT_NAME] ERROR Cannot change to source directory $SOURCE_DIR."
+  exit 1
+else
+  echo "[$SCRIPT_NAME] OK Now in source directory '$(pwd)'."
+fi
+
+#
+# Compile to byte code first
+#
+echo "[$SCRIPT_NAME] Compiling to byte code in module '$COMPILE_MODULE'..."
+if ! $PYTHON_BIN -m compileall "$COMPILE_MODULE"; then
+    echo "[$SCRIPT_NAME] ERROR Failed compilation!"
+    exit 1
+else
+    echo "[$SCRIPT_NAME] OK Compilation to byte code successful"
+fi
+
+export PYTHONIOENCODING=utf-8
+
+if [ $USE_GUNICORN -eq 0 ]; then
+  echo "[$SCRIPT_NAME] Starting $PROGRAM_NAME.."
+  PYTHONPATH="$PROJECTDIR"/"$SOURCE_DIR":"$EXTERNAL_SRC_FOLDERS" \
+     $PYTHON_BIN -m "$MODULE_TO_RUN" \
+       configfile="$CONFIGFILE" \
+       port="$PORT"
+else
+  echo "[$SCRIPT_NAME] NOT YET IMPLEMENTED $PROGRAM_NAME running from gunicorn."
+  exit 1
+  PYTHONPATH="$PROJECTDIR"/"$SOURCE_DIR":"$EXTERNAL_SRC_FOLDERS" \
+   $GUNICORN_BIN \
+      -w "$GUNICORN_WORKERS" -k "$GUNICORN_WORKER_TYPE" $GUNICORN_WORKER_TYPE_FLAG \
+      --bind 0.0.0.0:"$PORT" \
+         "$MODULE_TO_RUN" \
+            configfile="$CONFIGFILE" \
+            port="$PORT"
+fi
+
+if ! cd - ; then
+  echo "[$SCRIPT_NAME] ERROR Can't change back to original folder."
+  exit 1
+fi
